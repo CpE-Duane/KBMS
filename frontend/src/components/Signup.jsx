454ec8397135b4248cff8e8de2a48d389551bb13
Duane <villapando.duane@gmail.com>
2024-05-26 23:54:03 +0800

    x

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
-import { Container, Flex, Image, Button, Paper, TextInput, Center, Text, PasswordInput, SimpleGrid } from '@mantine/core';
+import React, { useState, useEffect } from 'react'
+import { Container, Flex, Image, Button, Paper, TextInput, Center, Text, PasswordInput, SimpleGrid, Modal, Divider, Stack, PinInput, Loader } from '@mantine/core';
 import mqcImg from '../assets/mqc.jpeg'
 import kbms from '../assets/kbms.png'
 import { useNavigate } from 'react-router-dom';
 import { isEmail, isNotEmpty, useForm } from '@mantine/form'
-import { Loader } from '@mantine/core';
 import AuthService from '../service/AuthService';
 import Toast from '../toast/Toast'
+import OtpService from '../service/OtpService';
 
 const SignUp = () => {
 
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
+    const [isVerifyModalOpen, setVerifyModalOpen] = useState(false)
+    const [values, setValues] = useState()
+    const [pin, setPin] = useState()
+    const [seconds, setSeconds] = useState(100);
+    const [started, setStarted] = useState(false);
 
     const form = useForm({
         mode: 'uncontrolled',
@@ -38,7 +43,24 @@ const SignUp = () => {
         }
     })
 
-    const handleSignUp = async (values) => {
+    const handleVerify = async () => {
+        try {
+            const payload = {
+                email: values.email,
+                otp: Number(pin)
+            }
+
+            const { data } = await OtpService.verifyOtp(payload)
+
+            await handleSignUp()
+        } catch (error) {
+            console.log("error", error);
+            Toast.errorMsg(error.response.data.message)
+        } finally {
+        }
+    }
+
+    const handleSignUp = async () => {
         try {
             setIsLoading(true);
             const res = await AuthService.signUpUser(values)
@@ -51,6 +73,36 @@ const SignUp = () => {
         }
     }
 
+    const generateOtp = async () => {
+        try {
+            setIsLoading(true)
+            const { data } = await OtpService.generateOtp({ email: values.email })
+            Toast.successMsg(data.message)
+            setStarted(true);
+        } catch (error) {
+            console.error("error", error)
+            Toast.errorMsg(error.resposne.data.message)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+
+    const openVerificationModal = async (values) => {
+        setValues(values)
+        setVerifyModalOpen(true)
+    }
+
+    useEffect(() => {
+        let interval;
+        if (started && seconds > 0) {
+            interval = setInterval(() => {
+                setSeconds(prevSeconds => prevSeconds - 1);
+            }, 1000);
+        }
+
+        return () => clearInterval(interval);
+    }, [started, seconds]);
 
     return (
         <>
@@ -59,6 +111,53 @@ const SignUp = () => {
                 fluid
                 style={{ height: "100vh" }}
             >
+
+                <Modal
+                    centered
+                    title="Verify Email"
+                    opened={isVerifyModalOpen}
+                    onClose={() => setVerifyModalOpen(false)}
+                >
+                    <Divider pb={5} />
+
+                    <TextInput
+                        label="Email"
+                        disabled
+                        value={values?.email}
+                    />
+
+                    <Stack align='center' mt={5}>
+                        <Button
+                            variant='light'
+                            color='red'
+                            onClick={generateOtp}
+                            disabled={started && seconds > 1}
+                        >
+                            {
+                                isLoading
+                                    ?
+                                    <>Sending code ... <Loader size={'xs'} color='red' /></>
+                                    :
+                                    <>Send Code {started && seconds > 0 ? seconds : ""}</>
+                            }
+                        </Button>
+                        <PinInput
+                            onChange={(value) => setPin(value)}
+                            inputType="tel"
+                            inputMode="numeric"
+                            disabled={!started}
+                        />
+                        <Button
+                            variant='light'
+                            onClick={handleVerify}
+                            color="green"
+                            disabled={!pin}
+                        >
+                            Verify
+                        </Button>
+                    </Stack>
+
+                </Modal>
                 <form style={{ height: "100%" }}>
                     <Container size={'xl'} h={'100%'}>
                         <Flex justify={'center'} align={'center'} h={'100%'}>
@@ -125,7 +224,12 @@ const SignUp = () => {
                                                 <Loader size={'xs'} color='violet' />
                                             </Button>
                                             :
-                                            <Button disabled={isLoading} variant="light" color="violet" onClick={form.onSubmit((values) => handleSignUp(values))}>
+                                            <Button
+                                                disabled={isLoading}
+                                                variant="light"
+                                                color="violet"
+                                                onClick={form.onSubmit((values) => openVerificationModal(values))}
+                                            >
                                                 SignUp
                                             </Button>
                                     }
